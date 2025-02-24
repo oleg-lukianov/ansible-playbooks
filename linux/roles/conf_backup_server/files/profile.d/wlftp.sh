feb62f692073ac30ace07f7c22f58d1f31c5ea06
Oleg Lukianov <oleg.lukianov@raiffeisen.ua>
2025-02-24 17:07:07 +0200

    feat backup script, git config, dir perm

--- linux/roles/conf_backup_server/files/profile.d/wlftp.sh
+++ linux/roles/conf_backup_server/files/profile.d/wlftp.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 
-pid=$(ps -ef | grep backup_from_sftp_to_hdd | grep -v grep | grep -v tee |  awk '{print $2}')
+pid=$(ps -ef | grep 'backup_from_sftp_to_hdd\|sleep' | grep -v grep | grep -v tee |  awk '{print $2}')
 
 echo ""
 echo "kill $pid"
 
 echo ""
-ps -ef | grep sleep | grep -v grep | grep -v tee
+ps -ef | grep 'backup_from_sftp_to_hdd\|sleep' | grep -v grep | grep -v tee
 
 echo ""
 echo ""
